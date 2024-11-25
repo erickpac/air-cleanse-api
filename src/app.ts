c28ece8219e8a26b2b630d89def34122a5b4d8ab
Erick Pac <erickj.pac@gmail.com>
2024-11-25 17:22:30 -0600

    Update package.json and app.ts: change main entry point to src/index.ts, add morgan middleware, and include vercel-build script

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import cors from "cors";
+import morgan from "morgan";
 import setRoutes from "./router";
 import * as middleware from "./middlewares";
 
 const app: Application = express();
 
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
