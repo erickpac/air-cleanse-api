ffc46ec5bb41926f911b911f2b469e9cdd0103b3
Erick Pac <erickj.pac@gmail.com>
2024-11-25 17:55:18 -0600

    Update dependencies in package.json and yarn.lock: upgrade @types/node, ts-node, and typescript to latest versions; remove vercel.json configuration file

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 import ErrorResponse from "./types/error-response";
 
@@ -15,8 +15,7 @@ export function errorHandler(
   next: NextFunction,
 ) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
   });
