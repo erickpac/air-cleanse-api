22577ca41cc2a08bc8adbe4888593cb63900e521
Erick Pac <erickj.pac@gmail.com>
2024-12-13 20:23:42 -0600

    refactor: export app instance and restore middleware usage in router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
+import "dotenv/config";
 import express, { Application } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import setRoutes from "@/router";
-import * as middleware from "@/middlewares";
 
-const app: Application = express();
+export const app: Application = express();
 
 app.use(morgan("combined"));
 app.use(express.json());
@@ -12,8 +12,3 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 setRoutes(app);
-
-app.use(middleware.notFound);
-app.use(middleware.errorHandler);
-
-export default app;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { Application, Router } from "express";
 import { router as ServerStatusRouter } from "@/components/server-status/network";
 import { router as PropertiesRouter } from "@/components/properties/network";
+import * as middleware from "@/middlewares";
 
 type Route = [string, Router];
 const routes: Route[] = [
@@ -12,6 +13,8 @@ const setRoutes = (app: Application) => {
   const router = Router();
 
   app.use("/api/v1", router);
+  app.use(middleware.notFound);
+  app.use(middleware.errorHandler);
 
   routes.forEach(([path, route]) => {
     router.use(`/${path}`, route);
