1bbc6782664b5bf19081f1eee448805b04247edb
Erick Pac <erickj.pac@gmail.com>
2024-12-17 13:45:26 -0600

    refactor: replace bcrypt and jwt usage with utility functions for password handling and token generation

--- src/components/auth/controller.ts
+++ src/components/auth/controller.ts
@@ -1,13 +1,11 @@
 import type { Request, Response } from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import * as service from "./service";
 import { normalizeError } from "@/utils/normalize-error";
 import { sendErrorResponse } from "@/common/responses/error";
 import { sendSuccessResponse } from "@/common/responses/success";
 import { CustomError } from "@/common/custom/error";
-import { JWT_SECRET } from "@/utils/config";
 import { Role } from "@prisma/client";
+import { generateToken, comparePassword, hashPassword } from "@/utils/auth";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -19,7 +17,7 @@ export const register = async (req: Request, res: Response) => {
       throw new CustomError("User already exists", 400);
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await hashPassword(password);
 
     await service.createUser({
       name,
@@ -45,17 +43,13 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     const user = await service.getUserByEmail(email);
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await comparePassword(password, user.password);
 
     if (!isMatch) {
       throw new CustomError("Invalid credentials", 400);
     }
 
-    const token = jwt.sign(
-      { userId: user.id, username: user.email },
-      JWT_SECRET as string,
-      { expiresIn: "1h" },
-    );
+    const token = generateToken(user.id, user.email);
 
     return sendSuccessResponse({ res, data: { token } });
   } catch (error) {
