5ac6cbf0f03f797a37cc05e6410cec961e6a295c
Erick Pac <erickj.pac@gmail.com>
2024-12-17 14:01:40 -0600

    feat: separate public and private routes in the router and enhance middleware usage

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,23 +2,30 @@ import { Application, Router } from "express";
 import { router as ServerStatusRouter } from "@/components/server-status/network";
 import { router as PropertiesRouter } from "@/components/properties/network";
 import { router as AuthRouter } from "@/components/auth/network";
-import * as middleware from "@/middlewares";
+import { notFound } from "@/middlewares/not-found";
+import { errorHandler } from "@/middlewares/error-handler";
+import { auth } from "@/middlewares/auth";
 
 type Route = [string, Router];
-const routes: Route[] = [
+const privateRoutes: Route[] = [
   ["server-status", ServerStatusRouter],
   ["properties", PropertiesRouter],
-  ["auth", AuthRouter],
 ];
+
+const publicRoutes: Route[] = [["auth", AuthRouter]];
 
 export const setRoutes = (app: Application) => {
   const router = Router();
 
   app.use("/api/v1", router);
-  app.use(middleware.notFound);
-  app.use(middleware.errorHandler);
+  app.use(notFound);
+  app.use(errorHandler);
 
-  routes.forEach(([path, route]) => {
+  privateRoutes.forEach(([path, route]) => {
+    router.use(`/${path}`, auth, route);
+  });
+
+  publicRoutes.forEach(([path, route]) => {
     router.use(`/${path}`, route);
   });
 };
