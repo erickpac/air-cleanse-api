03b849c40787bd5408bdc7e572a6e41c2ea87fa7
Erick Pac <erickj.pac@gmail.com>
2024-12-17 13:24:32 -0600

    fix: correct import casing for controller in authentication network

--- src/components/auth/network.ts
+++ src/components/auth/network.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import * as Controller from "./controller";
+import * as controller from "./controller";
 import { registerSchema, loginSchema } from "./validation-schema";
 import { validate } from "@/middlewares/zod-validation";
 
 export const router = Router();
 
-router.post("/register", validate(registerSchema), Controller.register);
-router.post("/login", validate(loginSchema), Controller.login);
+router.post("/register", validate(registerSchema), controller.register);
+router.post("/login", validate(loginSchema), controller.login);
